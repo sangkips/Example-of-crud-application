0c4b4c93bf8447dcf836f99b440c7ab5e14e226a
sangkips <sangkipkoech@gmail.com>
2022-08-24 08:48:52 +0300

    implemented static route

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,3 +1,4 @@
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -104,6 +105,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- project/models.py
+++ project/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=255)
+    featured_image = models.ImageField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
